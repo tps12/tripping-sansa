81eb902299e4e17a526602181adb38e1aae21df3
tps12 <tps12@columbia.edu>
2013-04-13 09:48:46 -0400

    Ajaxify the evens demo
      GET /app/evens/.*
    
    now returns a static HTML file that attempts to GET /evens/... as an
    AJAX request. The resource path is passed to the frontend app in a
    cookie.

--- src/app.c
+++ src/app.c
@@ -11,6 +11,7 @@
 #include "routing/functions/route_resource.h"
 
 #include "server/functions/build_entity.h"
+#include "server/functions/cookie.h"
 #include "server/functions/entity_reader.h"
 #include "server/functions/entity_writer.h"
 #include "server/functions/free_resource.h"
@@ -21,61 +22,47 @@
 #include "server/functions/resource_method.h"
 #include "server/functions/success_result.h"
 
-#define PAGE "<html><head><title>hello</title></head><body>there</body></html>"
-#define EVEN "<html><head><title>hello</title></head><body>%d</body></html>"
+#define EVEN "{ \"value\": %d }"
 
-static struct find_result* find_hello(char const* path, char const** args)
+static char* app_content = 0;
+
+static struct find_result* find_app(char const* path, char const** args)
 {
-    return found_resource(0, 0);
-}
+    char* data = 0;
 
-static struct result* get_hello(char const* path, void* resource_data, void* data)
-{
-    char* body = malloc(256);
+    if (args[0]) {
+        data = malloc(strlen(args[0]) + 1);
 
-    if (body) {
-      sprintf(body, PAGE);
-      return success_result(body, &free, 0);
+        if (data) {
+            strcpy(data, args[0]);
+            return found_resource(data, &free);
+        }
+        else
+            return 0;
     }
+    else
+        return resource_not_found();
+}
+
+static struct result* get_app(char const* path, void* value, void* data)
+{
+    if (value)
+        return success_result(app_content, 0, 0, cookie("resource", value, 0));
 
     return 0;
 }
 
-static struct result* put_hello(char const* path, void* resource_data, void* data)
-{
-    return success_result(0, 0, 0);
-}
-
-static struct result* post_hello(char const* path, void* resource_data, void* data)
-{
-    char* location = malloc(20);
-
-    if (location) {
-      sprintf(location, "/hello");
-      return success_result(0, 0, location);
-    }
-
-    return 0;
-}
-
-static void* read_hello(char const* entity, size_t length)
-{
-    return 0;
-}
-
-static struct entity* write_hello(void* data)
+static struct entity* write_app(void* data)
 {
     char* body = malloc(strlen((char*)data) + 1);
 
     if (body) {
-      strncpy(body, data, strlen((char*)data));
-      return build_entity(body);
+        strncpy(body, data, strlen((char*)data) + 1);
+        return build_entity(body);
     }
 
     return 0;
 }
-
-static struct resource* even;
 
 static struct find_result* find_even(char const* path, char const** args)
 {
@@ -105,7 +92,7 @@ static struct result* get_even(char const* path, void* value, void* data)
 
         if (body) {
             sprintf(body, EVEN, *((int*)value));
-            return success_result(body, &free, 0);
+            return success_result(body, &free, 0, 0);
         }
     }
 
@@ -124,6 +111,29 @@ static struct entity* write_even(void* data)
     return 0;
 }
 
+static char* read_file(char const* path)
+{
+    char* buffer = 0;
+    long size;
+    FILE* fp = fopen(path, "rb");
+
+    if (fp && fseek(fp, 0L, SEEK_END) == 0 && (size = ftell(fp)) != -1) {
+        if (buffer = malloc(size + 1)) {
+            rewind(fp);
+            if (size = fread(buffer, sizeof(char), size, fp))
+                buffer[size] = 0;
+            else {
+                free(buffer);
+                buffer = 0;
+            }
+        }
+    }
+
+    fclose(fp);
+
+    return buffer;
+}
+
 int main(int argc, char const** argv)
 {
     struct route* routes;
@@ -137,21 +147,23 @@ int main(int argc, char const** argv)
     port = atoi(argv[1]);
 
     routes =
-        path_route(route_pattern("^/hello$",
-            route_resource(resource_define(&find_hello,
-                resource_method("GET", 0, entity_writer("text/html", &write_hello, 0), &get_hello,
-                resource_method("PUT", entity_reader("text/plain", &read_hello, 0), 0, &put_hello,
-                resource_method("POST", 0, 0, &post_hello,
-            0)))),
+        path_route(route_pattern("^/app(/evens/.*)$",
+            route_resource(resource_define(&find_app,
+                resource_method("GET", 0, entity_writer("text/html", &write_app, 0), &get_app,
+            0)),
         0)),
         path_route(route_pattern("^/evens/([0-9]+)$",
             route_resource(resource_define(&find_even,
-                resource_method("GET", 0, entity_writer("text/html", &write_even, 0), &get_even,
+                resource_method("GET", 0, entity_writer("application/json", &write_even, 0), &get_even,
             0)),
         0)),
     0));
 
+    app_content = read_file("public/evens.html");
+
     run_app(port, routes);
+
+    free(app_content);
 
     free_routes(routes);
 
